1c5f16b9314d300334750e4dd60fc46eaf9fed6a
Ngo Quoc Khanh <137365902+khasnhnq@users.noreply.github.com>
2025-05-13 08:56:00 +0700

    Update task_tool.py

--- task_tool.py
+++ task_tool.py
@@ -13,8 +13,7 @@ def crawl_vnexpress_science():
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
-
+    
     # Khởi tạo trình điều khiển Chrome
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
