f109770228bc5e469ed5833d034bd9c98ed2ee05
Ngo Quoc Khanh <137365902+khasnhnq@users.noreply.github.com>
2025-05-13 08:56:28 +0700

    Update crawl_tool.py

--- crawl_tool.py
+++ crawl_tool.py
@@ -14,7 +14,6 @@ def crawl_vnexpress_health():
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
